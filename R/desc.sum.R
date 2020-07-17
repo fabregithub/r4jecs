9973a5bb7221bbfc65c883f8ff474664e96a7f5a
Shoji F. Nakayama <fabre@nies.go.jp>
2020-07-17 15:53:41 +0900

    updated

--- R/desc.sum.R
+++ R/desc.sum.R
@@ -12,10 +12,10 @@
 
 desc.sum <- function (data) {
   des <- describe(data)
-  l <- log10(x)
+  l <- log10(data)
   gm <- 10^mean(l)
   gsd <- 10^sd(l)
-  per <- quantile(x, c(0.25, 0.75, 0.95))
+  per <- quantile(data, c(0.25, 0.75, 0.95))
   res.0 <- data.frame(N = des[2], Min = des[8], P25 = per[1], Median = des[5], P75 = per[2],
                       P95 = per[3], Max = des[9], Mean = des[3], SD = des[4],
                       GM = gm, GSD = gsd
