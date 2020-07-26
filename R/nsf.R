4870cc1799903af94c2453a5d7590c2b13e9de57
Shoji Nakayama <fabre@nies.go.jp>
2020-07-26 13:38:09 +0900

    updated

--- R/nsf.R
+++ R/nsf.R
@@ -22,9 +22,9 @@
 #'
 
 nsf <- function(data, NSF, NBD){
-  x <- round(x, digits = NBD)
-  x <- signif(x, digits = NSF)
-  x <- ifelse(x > 1, formatC(x, digits = NSF, flag = '#', format = 'fg'), x)
-  x <- ifelse(grepl('\\.$', x), str_replace(x, '\\.', ''), x)
-  return(x)
+  res <- round(data, digits = NBD)
+  res <- signif(res, digits = NSF)
+  res <- ifelse(res > 1, formatC(res, digits = NSF, flag = '#', format = 'fg'), format(res, digits = NSF, nsmall = NBD, trim = TRUE))
+  res <- ifelse(grepl('\\.$', res), str_replace(res, '\\.', ''), res)
+  return(res)
 }
