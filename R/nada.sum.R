6e776ee1d390242c49191faeae0a45d9dbaf5696
Shoji F. Nakayama <fabre@nies.go.jp>
2020-11-19 14:34:48 +0900

    Code updated

--- R/nada.sum.R
+++ R/nada.sum.R
@@ -23,12 +23,20 @@ nada.sum <- function (data) {
     result <- list(summary = cf.summary, mean = mean(cf.res),
                    sd = sd(cf.res), quantile = quantile(cf.res), gm = 10^mean(l.cf.res)[1],
                    gsd = 10^sd(l.cf.res))
-    res.0 <- data.frame(N = result$summary$all[1], N.censored = result$summary$all[2],
-                        Percent.censored = result$summary$all[3], Min = result$summary$all[4],
-                        P25 = result$quantile[3], Median = result$quantile[4],
-                        P75 = result$quantile[5], P95 = result$quantile[7], Max = result$summary$all[5],
-                        Mean = result$mean[1], SD = result$sd, GM = result$gm,
-                        GSD = result$gsd)
+    res.0 <- data.frame(N = result$summary$all[1],
+                        N.censored = result$summary$all[2],
+                        Percent.censored = round(result$summary$all[3], 1),
+                        Percent.detected = round(100 - result$summary$all[3], 1),
+                        Min = nsf(result$summary$all[4], 3, 2),
+                        P25 = nsf(result$quantile[3], 3, 2),
+                        Median = nsf(result$quantile[4], 3, 2),
+                        P75 = nsf(result$quantile[5], 3, 2),
+                        P95 = nsf(result$quantile[7], 3, 2),
+                        Max = nsf(result$summary$all[5], 3, 2),
+                        Mean = nsf(result$mean[1], 3, 2),
+                        SD = nsf(result$sd, 3, 2),
+                        GM = nsf(result$gm, 3, 2),
+                        GSD = nsf(result$gsd3, 2))
     res[[i]] <- t(res.0)
     colnames(res[[i]]) <- clname
   }
